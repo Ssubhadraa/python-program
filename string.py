678500c0248ea3a782189f8c10a0c278f44fe0f6
subhadra <subhadradandapat18@gmail.com>
2026-07-01 00:34:23 +0530

    modified str file

--- string.py
+++ string.py
@@ -1,7 +1,15 @@
 str="subhadra"
 print(str)
 print(type(str))
+#indexing
 print(str[4])
+
 print(str[-1])
+#slicing
 print(str[2:5:1])
 print(str[::-1])
+print(str[1:788:1])
+print(str[1:3:0])
+
+
+
